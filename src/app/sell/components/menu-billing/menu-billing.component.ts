7373167ae4935f86b52e6919412d58e9b70594bd
Danny Pacheco <dpac4796@gmail.com>
2022-03-23 20:08:52 -0500

    improvements UI

--- src/app/sell/components/menu-billing/menu-billing.component.ts
+++ src/app/sell/components/menu-billing/menu-billing.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { User } from 'src/app/interfaces/User';
+import { UserServicesService } from 'src/app/services/user-services.service';
 
 @Component({
   selector: 'app-menu-billing',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-billing.component.scss']
 })
 export class MenuBillingComponent implements OnInit {
+  public user!:User;
+  public menuDataStatus:boolean = false;
 
-  constructor() { }
+  constructor(
+    private _userService:UserServicesService,
+    private router:Router) {
+
+    }
 
   ngOnInit(): void {
+    this.user = this._userService.initUser();
+    this.verifiyLoggedUser();
+  }
+
+  private verifiyLoggedUser(){
+    const aux:any= localStorage.getItem('user');
+    if(aux){
+      this.user = JSON.parse(aux);
+      this.menuDataStatus = true;
+    }else{
+      this.menuDataStatus = false;
+    }
+  }
+
+
+  public logOut(){
+    localStorage.clear();
+    this.menuDataStatus = false;
+    this.router.navigateByUrl('');
   }
 
 }
