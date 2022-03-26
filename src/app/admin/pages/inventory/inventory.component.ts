4375c5d4eb081bfeac45029124dd5856d35f4156
Danny Pacheco <65546712+dapacheco1@users.noreply.github.com>
2022-03-26 13:14:34 -0500

    improvements on inventory register

--- src/app/admin/pages/inventory/inventory.component.ts
+++ src/app/admin/pages/inventory/inventory.component.ts
@@ -5,6 +5,7 @@ import { Gender } from 'src/app/interfaces/Gender';
 import { Size } from 'src/app/interfaces/Size';
 import { CategoriesService } from 'src/app/services/categories.service';
 import { ClothingService } from 'src/app/services/clothing.service';
+import { FormValidationsService } from 'src/app/services/form-validations.service';
 import { GenderService } from 'src/app/services/gender.service';
 import { SizeService } from 'src/app/services/size.service';
 
@@ -25,7 +26,8 @@ export class InventoryComponent implements OnInit {
     private _catServices:CategoriesService,
     private _sizeServices:SizeService,
     private _genderServices:GenderService,
-    private _cltServices:ClothingService
+    private _cltServices:ClothingService,
+    private _form:FormValidationsService
   ) {
     
    }
@@ -71,15 +73,51 @@ export class InventoryComponent implements OnInit {
   }
 
   getSizeId(event:any){
+
     this.clothe.size_id = event.target.value;
   }
 
   getGenderId(event:any){
     this.clothe.genre_id = event.target.value;
+    
   }
 
   saveClothe(){
-    console.log(this.clothe);
-    this._cltServices.createClothe(this.clothe).subscribe(res=>console.log(res));
+    if(this.validateForm()){
+      this._cltServices.createClothe(this.clothe).subscribe(res=>{
+        if(res.success){
+          this.resetForm();
+        }else{
+          alert(res.message);
+        }
+      });
+    }
+    
+  }
+
+  resetForm(){
+    this.clothe = this._cltServices.initClothing();
+    this.cats = [];
+    this.sz = [];
+    this.genders = [];
+    this.clothe = this._cltServices.initClothing();
+    this.loadCategories();
+    this.loadSizes();
+    this.loadGenders();
+  }
+
+
+
+  validateForm(){
+    const price = this._form.onlyNumbers((String)(this.clothe.price),"price");
+    const stock = this._form.onlyNumbers((String)(this.clothe.stock),"stock");
+    const name = this._form.onlyLetters(this.clothe.name,"product name");
+    const details = this._form.onlyLettersSpecialLettersAndNumbers(this.clothe.detail,"details");
+    
+    if(price.success && stock.success && name.success && details.success && this.clothe.size_id != 0 && this.clothe.genre_id != 0 && this.clothe.category_id!=0){
+      return true;
+    }else{
+      return false;
+    }
   }
 }
