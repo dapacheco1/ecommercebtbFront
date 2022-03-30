b26354b6be7196d11752eb61286849b995d6430d
Danny Pacheco <65546712+dapacheco1@users.noreply.github.com>
2022-03-30 12:14:24 -0500

    old

--- src/app/admin/pages/reports/reports.component.ts
+++ src/app/admin/pages/reports/reports.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SalesDetail } from 'src/app/interfaces/SalesDetails';
 import { ReportsService } from 'src/app/services/reports.service';
 import { SalesDetailService } from 'src/app/services/sales-detail.service';
+import {Chart} from 'chart.js';
 
 @Component({
   selector: 'app-reports',
@@ -18,7 +19,41 @@ export class ReportsComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    
+    const ctx:any = document.getElementById('myChart');
+    const myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          datasets: [{
+              label: '# of Votes',
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          }
+      }
+  });
   }
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-
+import {ChartsModule} from 'ng2-charts';
 
 
 
